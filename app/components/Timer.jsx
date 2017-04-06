9ff8a9676987fd0faa33bd8c785639c7dcf3950c
Chun Shien <chunshienng@gmail.com>
2017-04-06 16:41:02 +0800

    foundation settings

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,73 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+var TimerForm = require('TimerForm');
 
 var Timer = React.createClass({
+  getInitialState: function(){
+    return {
+      count:0,
+      timerStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function(prevProps, prevState){
+    if(this.state.timerStatus !== prevState.timerStatus){
+      switch(this.state.timerStatus){
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({
+            count:0
+          });
+          break;
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function(){
+    //console.log('will unmount');
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function(){
+    this.timer = setInterval(()=>{
+      var newCount = this.state.count + 1;
+
+      this.setState({
+        count: newCount > 0 ? newCount : 0
+      });
+
+      // if(newCount === 60){
+      //   this.setState({
+      //     countdownStatus: 'stopped'
+      //   });
+      // }
+
+    }, 1000);
+  },
+  handleSetTimer: function(seconds){
+    this.setState({
+      count: seconds,
+      timerStatus: 'started'
+    });
+  },
+  handleStatusChange: function(newStatus){
+    this.setState({
+      timerStatus: newStatus
+    });
+  },
   render: function(){
+    var {count, timerStatus} = this.state;
     return (
-      <p>Timer.jsx</p>
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds = {count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange} />
+      </div>
     );
   }
 });
--- app/components/TimerForm.jsx
+++ app/components/TimerForm.jsx
@@ -1,20 +1,20 @@
 var React = require('react');
 
-var CountdownForm = React.createClass({
+var TimerForm = React.createClass({
   onSubmit:function(e){
     e.preventDefault();
 
     var strSeconds = this.refs.seconds.value;
-    console.log('input count',$('input').length);
+
     if(strSeconds.match(/^[0-9]*$/)){
       this.refs.seconds.value = '';
-      this.props.onSetCountdown(parseInt(strSeconds, 10));
+      this.props.onSetTimer(parseInt(strSeconds, 10));
     }
   },
   render: function(){
     return (
       <div>
-        <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
+        <form ref="form" onSubmit={this.onSubmit} className="timer-form">
           <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
           <button className="button expanded">Start</button>
         </form>
@@ -23,4 +23,4 @@ var CountdownForm = React.createClass({
   }
 });
 
-module.exports = CountdownForm;
+module.exports = TimerForm;
